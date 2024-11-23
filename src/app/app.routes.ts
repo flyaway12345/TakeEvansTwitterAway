39578fb4bfef61030f278f619a27fd568e03f7a4
flyaway12345 <evanhaggard123@gmail.com>
2024-11-23 11:38:28 -0800

    data implementation attempt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { Error404Component } from '../error404/error404.component';
 import { DisplayTweetComponent } from '../display-tweet/display-tweet.component';
 
 export const routes: Routes = [
--- src/display-tweet/display-tweet.component.ts
+++ src/display-tweet/display-tweet.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { GetDataService } from './get-data.service';
 
 @Component({
   selector: 'app-display-tweet',
@@ -8,5 +12,18 @@ import { Component } from '@angular/core';
   styleUrl: './display-tweet.component.css'
 })
 export class DisplayTweetComponent {
-   test = "test";
+   tweetArray:string[] = [];
+   tweetList: Array<any> = [];
+   constructor(private getDataService: GetDataService) { }
+
+   public baseData() {
+    this.getDataService.getData().subscribe(res => {
+      this.tweetList = res;
+      console.log("data aquired");
+    });
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
