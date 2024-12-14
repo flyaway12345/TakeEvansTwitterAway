3b6845f32353646e793578d57e45a559d2daa0b8
flyaway12345 <evanhaggard123@gmail.com>
2024-12-14 12:20:37 -0800

    updated entries removed bloat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
-import { DisplayTweetComponent } from "../display-tweet/display-tweet.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, DisplayTweetComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { DisplayTweetComponent } from '../display-tweet/display-tweet.component';
-
 export const routes: Routes = [
-  {path: '**' ,component: DisplayTweetComponent}
 ];
