93aa25e67c20c334e3e255e662e79179b3c9ac01
flyaway12345 <evanhaggard123@gmail.com>
2024-12-14 10:47:19 -0800

    added first round of tweets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
+import { DisplayTweetComponent } from "../display-tweet/display-tweet.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, DisplayTweetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/display-tweet/display-tweet.component.ts
+++ src/display-tweet/display-tweet.component.ts
@@ -1,8 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component } from '@angular/core';
-import { map } from 'rxjs';
-import { Observable } from 'rxjs/internal/Observable';
-import { GetDataService } from './get-data.service';
+
 
 @Component({
   selector: 'app-display-tweet',
@@ -12,18 +10,7 @@ import { GetDataService } from './get-data.service';
   styleUrl: './display-tweet.component.css'
 })
 export class DisplayTweetComponent {
-   tweetArray:string[] = [];
-   tweetList: Array<any> = [];
-   constructor(private getDataService: GetDataService) { }
 
-   public baseData() {
-    this.getDataService.getData().subscribe(res => {
-      this.tweetList = res;
-      console.log("data aquired");
-    });
-  }
+   constructor() { }
 
-  ngOnInit(): void {
-    
-  }
 }
